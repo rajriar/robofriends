0eee43658d515e1cb34ccd3447732a7c9ed9f977
rajriar <ucraj510@gmail.com>
2020-01-08 14:33:09 -0800

    Updates

---
+++ precache-manifest.7ef2a97439817af2d8963f682102149d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20258898b8d359ced5d459dee0cd8bd9",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "1f06a453f36720339c4c",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8d3aea57b730ff268e66",
+    "url": "/robofriends/static/css/main.15a928ee.chunk.css"
+  },
+  {
+    "revision": "1f06a453f36720339c4c",
+    "url": "/robofriends/static/js/2.38b44fc0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.38b44fc0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d3aea57b730ff268e66",
+    "url": "/robofriends/static/js/main.45e144d2.chunk.js"
+  },
+  {
+    "revision": "0d000ca805ddaefdf0fc",
+    "url": "/robofriends/static/js/runtime-main.8ebeaa0d.js"
+  }
+]);
