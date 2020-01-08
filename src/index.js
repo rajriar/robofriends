4cef57430e7573538a552777b9d4c5a3fcf38faf
rajriar <ucraj510@gmail.com>
2020-01-07 19:06:55 -0800

    Added Cards

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { robots } from './robots';
 
-ReactDOM.render( < Hello greeting = { 'Hello React Ninja' }
-        /> , document.getElementById('root'));
+ReactDOM.render(
+    <div>
+       <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} / >
+       <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email} / >
+       <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email} / >
+    </div>
+    , document.getElementById('root'));
 
-        // If you want your app to work offline and load faster, you can change
-        // unregister() to register() below. Note this comes with some pitfalls.
-        // Learn more about service workers: https://bit.ly/CRA-PWA
-        serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
